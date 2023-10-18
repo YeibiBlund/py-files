f97283b09bc5d35eaec5bf8707beadc211b5518c
Bruno Cuevas <brunocue92@gmail.com>
2023-10-18 17:15:58 +0200

    ejercicios finalizados

--- index.py
+++ index.py
@@ -1,21 +1,24 @@
-# import your functions here
+# import your functions here    //   IMPORTAR LA CLASE DONDE ESTAN LAS FUNCIONES
 from utils import files
 
-# read the quijote here
+# read the quijote here    // LEER ARCHIVO TXT Y PASARLO A CADENA 
 book = files.readFile('el_quijote.txt')
 book2 = files.readFile('el_quijote_ii.txt')
 bookCompleto = book+book2
 
-# Word Count
+print('Libro Completo: ', bookCompleto)
+
+# Word Count    // CONTADOR DE PALABRAS TOTALES   .wordcount
 print('Palabras totales: ', files.wordCount(bookCompleto))
 
+# Unique Word Count    // CONTADOR DE PALABRAS UNICAS 
+print('\nPalabras Unicas: ', files.uniqueWordCount(bookCompleto))
 
-# Unique Word Count
-# print('Unique Word Count: ', files.uniqueWordCount(book))
+# Quijote count    //CONTADOR DE PALABRAS EN CONCRETO  "QUIJOTE" "SANCHO"
+quijote, sancho = files.quijote_count(bookCompleto)
+print('\nVeces que se utiliza la palabra QUIJOTE: ', quijote)
+print('Veces que se utiliza la palabra SANCHO: ', sancho)
 
-# Quijote count
-# print('find Content: ', files.findContent(book, 'quijote'))
-# print('find Content: ', files.findContent(book, 'sancho'))
-
-# Change Quijote to Quixote and write it to a new file "el_quixote.txt"
-# print('Change Quijote to Quixote: ', files.changeQuijoteToQuixote(book))
+# Change Quijote to Quixote and write it to a new file "el_quixote.txt"   //COPIAR TEXTO Y CREAR UN ARCHIVO TXT
+files.modificarYcrearTxt(bookCompleto)
+print('Change Quijote to Quixote: True')
--- utils/files.py
+++ utils/files.py
@@ -1,15 +1,39 @@
 def readFile(name):
     with open(name, "r", errors="ignore") as file:  
-        return file.read()
+        return file.read()    #  .read  LEE EL CONTENIDO DEL ARCHIVO ESPECIFICADO Y LO DEVUELVE COMO CADENA.
 
 def wordCount(text):
   palabras_totales = len(text.split())
   return palabras_totales
 
 def uniqueWordCount(text):
-  # return count
-  return 0
+  palabras_totales = text.split()     #  .split  SEPARA UNA UNA CADENA DE TEXTO Y LA CONVIERTE EN UNA LISTA(ARRAY)
+  palabras_unicas = set(palabras_totales)
+  return len(palabras_unicas)
 
 def findContent(text, word):
-  # return count
-  return 0
+    palabras_totales = text.split()      #  .split  SEPARA UNA UNA CADENA DE TEXTO Y LA CONVIERTE EN UNA LISTA(ARRAY)
+    total_palabras = palabras_totales.count(word)     #  .count  CUENTA EL NUMERO DE VECES QUE APARECE UN ELEMENTO EN UNA LISTA
+    return total_palabras
+
+def quijote_count(text):
+    #text = text.lower()      # .lower  CONVIERTE UN TEXTO A MINUSCULAS. (NO ES NECESARIO USARLO PORQUE QUIJOTE Y SANCHO SIEMPRE VAN EN MAYUSCULAS)
+    
+    quijote_count = text.count("Quijote")
+    sancho_count = text.count("Sancho")
+    
+    return quijote_count, sancho_count
+
+
+def modificarYcrearTxt(test):
+   bookCompleto_modificado = test.replace("Quijote", "Quixote")
+   with open('el_quixote.txt', 'w', encoding='utf-8') as new_file:
+    new_file.write(bookCompleto_modificado)
+   return
+
+
+
+#  .read   |  LEE EL CONTENIDO DEL ARCHIVO ESPECIFICADO Y LO DEVUELVE COMO CADENA.
+#  .split  |  SEPARA UNA UNA CADENA DE TEXTO Y LA CONVIERTE EN UNA LISTA(ARRAY)
+#  .count  |  CUENTA EL NUMERO DE VECES QUE APARECE UN ELEMENTO EN UNA LISTA
+#  .lower  |  CONVIERTE UN TEXTO A MINUSCULAS. QUIJOTE:  2203   
